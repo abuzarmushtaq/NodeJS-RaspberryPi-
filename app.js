59fd2d12cbeed87d636d6e6641bb5f9a793342dc
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2018-01-05 00:55:17 +0500

    fixes

--- app.js
+++ app.js
@@ -9,31 +9,31 @@ var LED = new Gpio(4, 'out');
 
 http.listen(8080); //listen to port 8080
 
-function handler (req, res) { 
-  fs.readFile(__dirname + '/index.html', function(err, data) { 
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); 
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+function handler(req, res) {
+    fs.readFile('index.html', function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
 }
 
 io.sockets.on('connection', function (socket) {
-  var lightvalue = 0;
+    var lightvalue = 0;
 
-  socket.on('light', function(data) {
-    lightvalue = data;
-    if (lightvalue != LED.readSync()) { 
-      LED.writeSync(lightvalue);
-    }
-  });
+    socket.on('light', function (data) {
+        lightvalue = data;
+        if (lightvalue != LED.readSync()) {
+            LED.writeSync(lightvalue);
+        }
+    });
 });
 
-process.on('SIGINT', function () { 
-  LED.writeSync(0);
-  LED.unexport(); 
-  process.exit();
+process.on('SIGINT', function () {
+    LED.writeSync(0);
+    LED.unexport();
+    process.exit();
 }); 
