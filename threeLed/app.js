ee79ecbe06ff5fa95b868a773cee3eb4f6ace5b0
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2018-01-08 01:38:16 +0500

    features added

--- threeLed/app.js
+++ threeLed/app.js
@@ -5,7 +5,9 @@ var fs = require('fs');
 var io = require('socket.io')(http);
 var Gpio = require('onoff').Gpio;
 
-var LED = new Gpio(4, 'out');
+var switch1 = new Gpio(4, 'out');
+var switch2 = new Gpio(11,out);
+var switch3 = new Gpio(26,out);
 
 
 http.listen(8080); //listen to port 8080
@@ -25,16 +27,41 @@ function handler(req, res) {
 io.sockets.on('connection', function (socket) {
     var lightvalue = 0;
 
-    socket.on('light', function (data) {
+    // light 1
+    socket.on('light1', function (data) {
         lightvalue = data;
-        if (lightvalue != LED.readSync()) {
-            LED.writeSync(lightvalue);
+        if (lightvalue != switch1.readSync()) {
+            switch1.writeSync(lightvalue);
+        }
+    });
+
+    // light 2
+    socket.on('light2', function (data) {
+        lightvalue = data;
+        if (lightvalue != switch2.readSync()) {
+            switch2.writeSync(lightvalue);
+        }
+    });
+
+    // light 3
+    socket.on('light3', function (data) {
+        lightvalue = data;
+        if (lightvalue != switch3.readSync()) {
+            switch3.writeSync(lightvalue);
         }
     });
 });
 
 process.on('SIGINT', function () {
-    LED.writeSync(0);
-    LED.unexport();
+    
+    switch1.writeSync(0);
+    switch1.unexport();
+
+    switch2.writeSync(0);
+    switch2.unexport();
+
+    switch3.writeSync(0);
+    switch3.unexport();
+    
     process.exit();
 }); 
